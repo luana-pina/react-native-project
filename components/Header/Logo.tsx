8cf0018e5b7325efa005c4d915598a177d3118e2
Luana Pina <83229362+luana-pina@users.noreply.github.com>
2022-04-04 23:26:02 -0300

    feat: Layout base and colors app

---
+++ components/Header/Logo.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Colors } from "../../src/shared/constants/colors";
+import { AntDesign } from "@expo/vector-icons";
+
+const Logo: React.FC = () => {
+  return (
+    <>
+      <View style={style.logoContainer}>
+        <Text style={style.text}>TGL</Text>
+        <View style={style.line} />
+      </View>
+    </>
+  );
+};
+
+export default Logo;
+
+const style = StyleSheet.create({
+  logoContainer: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  line: {
+    backgroundColor: Colors.green400,
+    width: 55,
+    height: 4,
+    borderRadius: 2,
+  },
+  text: {
+    fontWeight: "bold",
+    color: Colors.gray800,
+    fontSize: 25,
+  },
+});
---
+++ src/shared/constants/colors.ts
@@ -0,0 +1,13 @@
+export const Colors = {
+  white: "#ffffff",
+  background400: "#f7f7f7",
+  background500: "#F4F4F4",
+  background600: "#EBEBEB",
+  background700: "#E2E2E2",
+  green400: "#B5C401",
+  green500: "#27C383",
+  gray800: "#707070",
+  gray700: "#868686",
+  gray600: "#888888",
+  grayBlue: "#ADC0C4",
+};
