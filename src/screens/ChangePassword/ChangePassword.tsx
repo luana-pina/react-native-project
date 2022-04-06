6c7e4bfb2ff523bf61d8b053577fbc9f4467fb13
Luana Pina <83229362+luana-pina@users.noreply.github.com>
2022-04-06 16:07:56 -0300

    feat: finished cheange password page layout

--- src/screens/ChangePassword/ChangePassword.tsx
+++ src/screens/ChangePassword/ChangePassword.tsx
@@ -1,11 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
 import Base from "../../../components/Base/Base";
+import Input from "../../../components/Input/Input";
 import AuthLayout from "../../../components/Layout/AuthLayout";
+import AuthButton from "../../../components/UI/Butons/AuthButton";
 import { IStackScreenProps } from "../../shared/interfaces/NavigationProps";
 
 const ChangePassword: React.FunctionComponent<IStackScreenProps> = ({
   navigation,
 }) => {
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredConfirmPassword, setEnteredConfirmPassword] = useState("");
+
+  function changePasswordHandler() {
+    console.log(enteredPassword, enteredConfirmPassword);
+    setEnteredConfirmPassword("");
+    setEnteredPassword("");
+    navigation.navigate("Login");
+  }
+
   return (
     <Base>
       <AuthLayout
@@ -13,9 +26,39 @@ const ChangePassword: React.FunctionComponent<IStackScreenProps> = ({
         onPress={() => {
           navigation.goBack();
         }}
-      ></AuthLayout>
+      >
+        <View style={styles.container}>
+          <Input
+            label="Password"
+            value={enteredPassword}
+            secure
+            isInvalid={false}
+            onUpdateValue={(enteredValue: string) => {
+              setEnteredPassword(enteredValue);
+            }}
+            noPaddingTop
+          />
+          <Input
+            label="Confirm password"
+            value={enteredConfirmPassword}
+            secure
+            isInvalid={false}
+            onUpdateValue={(enteredValue: string) => {
+              setEnteredConfirmPassword(enteredValue);
+            }}
+          />
+          <AuthButton text="Change" onPress={changePasswordHandler} />
+        </View>
+      </AuthLayout>
     </Base>
   );
 };
 
 export default ChangePassword;
+
+const styles = StyleSheet.create({
+  container: {
+    width: 250,
+    alignItems: "center",
+  },
+});
