579dee0775320da3e97c894fc0f67699bc9ed788
Luana Pina <83229362+luana-pina@users.noreply.github.com>
2022-04-20 13:24:00 -0300

    fix: babel plugin error fixed

--- babel.config.js
+++ babel.config.js
@@ -3,19 +3,21 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "module-resolver",
-      {
-        root: ["./src"],
-        extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
-        alias: {
-          "@components": "./src/components",
-          "@constants": "./src/shared/constants",
-          "@interfaces": "./src/shared/interfaces",
-          "@providers": "./src/shared/providers",
-          "@store": "./src/shared/store",
-          "@utils": "./src/shared/utils",
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+          alias: {
+            "@components": "./src/components",
+            "@constants": "./src/shared/constants",
+            "@interfaces": "./src/shared/interfaces",
+            "@providers": "./src/shared/providers",
+            "@store": "./src/shared/store",
+            "@utils": "./src/shared/utils",
+          },
         },
-      },
+      ],
     ],
   };
 };
